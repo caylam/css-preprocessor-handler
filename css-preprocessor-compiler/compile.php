0092e455b46f3246f854f05b90809e59331fe2b3
Caylam <consulting@caylam.com>
2012-06-14 18:02:14 +0100

    Added commands to compile sass, scss and stylus
    added handlers for sass and stylus

--- css-preprocessor-compiler/compile.php
+++ css-preprocessor-compiler/compile.php
@@ -1,26 +1,45 @@
 <?php
 $cachedir = '/tmp/css-preprocessor-cache/';
-$prefix = 'less-';
-$suffix = '.css';
+$cachePrefix = '';
+$cacheSuffix = '.css';
 
-if(!is_dir($cachedir)){
+$cssCompilerCommand = '';
+
+if(!is_dir($cachedir)) {
     mkdir($cachedir, 755, true);
 }
 
-$hash = substr(exec("/usr/bin/env sha1sum ". $_SERVER['PATH_TRANSLATED']), 0, 40);
-$cache_req = $cachedir . $prefix . $hash . $suffix;
+if(preg_match("/.less$/", $_SERVER['PATH_TRANSLATED'])) {
 
-function compile_less() {
-	$cmd = "/usr/bin/env lessc ". $_SERVER['PATH_TRANSLATED'] . " 2>&1; echo $?";
-	$compiled = trim(`$cmd`);
-	$exit_code = (int) substr($compiled,-1);
-	$compiled = substr($compiled,0,-1);
-	if (0 !== $exit_code) { throw new Exception($compiled); }
-	return $compiled;
+    $cssCompilerCommand = "/usr/bin/env lessc ". $_SERVER['PATH_TRANSLATED'] . " 2>&1; echo $?";
+    $cachePrefix = 'less-';
+
+}
+else if(preg_match("/.sass$/", $_SERVER['PATH_TRANSLATED'])
+    || preg_match("/.scss$/", $_SERVER['PATH_TRANSLATED'])) {
+
+    $cssCompilerCommand = "/usr/bin/env sass ". $_SERVER['PATH_TRANSLATED'] . " 2>&1; echo $?";
+    $cachePrefix = 'sass-';
+
+}
+else if(preg_match("/.styl$/", $_SERVER['PATH_TRANSLATED'])) {
+
+    $cssCompilerCommand = "/usr/bin/env stylus ". $_SERVER['PATH_TRANSLATED'] . " 2>&1; echo $?";
+    $cachePrefix = 'stylus-';
+
 }
 
-function compile_sass() {
+$hash = substr(exec("/usr/bin/env sha1sum ". $_SERVER['PATH_TRANSLATED']), 0, 40);
+$cache_req = $cachedir . $cachePrefix . $hash . $cacheSuffix;
 
+
+function compile_css($cssCompilerCommand){
+    $cmd = $cssCompilerCommand;
+    $compiled = trim(`$cmd`);
+    $exit_code = (int) substr($compiled,-1);
+    $compiled = substr($compiled,0,-1);
+    if (0 !== $exit_code) { throw new Exception($compiled); }
+    return $compiled;
 }
 
 if (isset($_GET['recache'])) {
@@ -34,13 +53,13 @@ try {
 		exit(0);
 	} elseif (isset($_GET['nocache'])) {
 		header('Content-type: text/css;');
-		$compiled = compile_less();
+		$compiled = compile_css($cssCompilerCommand);
 		echo $compiled;
 		exit(0);
 	} else {
 		header('Content-type: text/css;');
 		if (! is_readable($cache_req)) {
-			$compiled = compile_less();
+			$compiled = compile_css($cssCompilerCommand);
 			file_put_contents($cache_req, $compiled);
 		}
 		echo file_get_contents($cache_req);
